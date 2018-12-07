aeb018e6cd5ffd2fb8358f3cb1d2945987422451
Reed Rizzo <reed.rizzo@coatesgroup.com>
2018-12-07 09:33:02 -0600

    Check in

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var colors = require('colors');
 const PORT = process.env.PORT || 5000
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function (socket) {
+  console.log('a user connected'.green);
 
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-        console.log('message: ' + msg);
-    });
-
-    socket.on('contentcontroller', function(data){
-        io.emit('contentcontroller', data);
-    });
-
-    socket.on('reedonly', function(data){
-      io.emit('reedonly', data);
-    });
-
-    socket.on('dev1', function(data){
-      io.emit('dev1', data);
-    });
-
-    socket.on('dev2', function(data){
-      io.emit('dev2', data);
-    });
-
-    socket.on('dev3', function(data){
-      io.emit('dev3', data);
-    });
-
-    socket.on('demo1', function(data){
-      io.emit('demo1', data);
-    });
-
-    socket.on('demo2', function(data){
-      io.emit('demo2', data);
-    });
-
-    socket.on('demo3', function(data){
-      io.emit('demo3', data);
-    });
-
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
   });
 
+  socket.on('contentcontroller', function (data) {
+    console.log('')
+    console.log(`Action: ${data.action}`.blue);
+    console.log('Channel: contentcontroller'.green);
+    console.log(`SubChannel: ${data.subchannel}`.green);
+    console.log('data: ' + JSON.stringify(data));
+    console.log('')
+    io.emit('contentcontroller', data);
+  });
 
-http.listen(PORT, function(){
+});
+
+http.listen(PORT, function () {
   console.log(`listening on *:${PORT}`);
 });
